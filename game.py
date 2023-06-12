aa96c92c61346200fde0a2bbfa5133fa72a69c73
Victor <yangv140@gmail.com>
2023-06-12 15:50:40 +0200

    commit

--- game.py
+++ game.py
@@ -24,13 +24,13 @@ class Map:
         # Définir la taille de la matrice et des carrés
         self.matrix_width = len(self.word[0])
         self.matrix_height = len(self.word)
+        self.fenetre = pygame.display.set_mode((self.matrix_width, self.matrix_height))
         # la taille de chaque cellule dans le fenetre qu'on met  en pixels
         self.pixels = 40
         # Définir la taille de la fenêtre en fonction de notre matrice
         self.window_width = self.matrix_width * self.pixels
         self.window_height = self.matrix_height * self.pixels
-        self.screen = pygame.display.set_mode(
-            (self.window_width, self.window_height))
+        self.screen = pygame.display.set_mode((self.window_width, self.window_height))
         # charger un image de fond
         self.background = pygame.image.load("Assets/sable.jpg")
         self.background = pygame.transform.scale(self.background, (self.window_width, self.window_height))
@@ -96,6 +96,43 @@ class Map:
         self.arme_initiale = None
         self.type_arme = ""
         self.prix_arme_definie = None
+
+        #-------------------------------------
+        self.vitesse_monstre = 5
+        self.horloge = pygame.time.Clock()
+
+        self.image_monstre = pygame.image.load('Assets/Monsters/Turtle_monster.png')
+        self.image_monstre = pygame.transform.scale(self.image_monstre, (self.pixels, self.pixels))
+
+        self.image_mur = pygame.image.load('Assets/Armes/wals.png')
+        self.image_mur = pygame.transform.scale(self.image_mur, (self.pixels, self.pixels))
+
+        self.image_chemin = pygame.image.load('Assets/sable.jpg')
+        self.image_chemin = pygame.transform.scale(self.image_chemin, (self.pixels, self.pixels))
+
+        self.image_menu = pygame.image.load("Assets/Armes/Image 89 at frame 1copy .jpg")
+        self.image_menu = pygame.transform.scale(self.image_menu, (self.pixels, self.pixels))
+
+        self.pos_monstre = [11, 1]
+        self.positions_visitees = []
+        
+        self.terrain = [
+
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
+
+        ]
 
     def draw(self):
         # Afficher les armes disponibles
@@ -207,6 +244,25 @@ class Map:
             if monstre.positionX == condition_x and monstre.positionY == condition_y:
                 self.vie_joueur.degat(monstre.degat, self.screen)
 
+    def est_position_valide(self, position):
+        x, y = position
+        return 0 <= x < len(self.terrain) and 0 <= y < len(self.terrain[0]) and self.terrain[x][y] == 0
+
+    def deplacer(self):
+        x, y = self.pos_monstre
+
+        if x - 1 >= 0 and self.est_position_valide([x - 1, y]) and [x - 1, y] not in self.positions_visitees:
+            self.pos_monstre = [x - 1, y]  # Déplacer vers le haut
+
+        elif y + 1 < len(self.terrain[0]) and self.est_position_valide([x, y + 1]) and [x, y + 1] not in self.positions_visitees:
+            self.pos_monstre = [x, y + 1]  # Déplacer vers la droite
+
+        elif x + 1 < len(self.terrain) and self.est_position_valide([x + 1, y]) and [x + 1, y] not in self.positions_visitees:
+            self.pos_monstre = [x + 1, y]  # Déplacer vers le bas
+
+        elif y - 1 >= 0 and self.est_position_valide([x, y - 1]) and [x, y - 1] not in self.positions_visitees:
+            self.pos_monstre = [x, y - 1]  # Déplacer vers la gauche
+
     def run(self):
 
         while self.running:
@@ -228,11 +284,36 @@ class Map:
 
             # Map 1
             elif self.etat == "jeu_map1":
-                self.maps(165, -78, 165, -69, word, 1)
-                self.verifier_le_click_sur_quel_image(word)
-                self.draw()
-                self.vie_joueur.afficher_vie_joueur(self.screen)
+                # self.maps(165, -78, 165, -69, word, 1)
+                # self.verifier_le_click_sur_quel_image(word)
+                # self.draw()
+                # self.vie_joueur.afficher_vie_joueur(self.screen)
+                # pygame.display.update()
+
+                #--------------------------------------------------
+
+                self.deplacer()
+                self.positions_visitees.append(self.pos_monstre)
+
+                for i in range(len(self.terrain)):
+                    for j in range(len(self.terrain[0])):
+
+                        if self.terrain[i][j] == 0:
+                            rect = pygame.Rect(j * self.pixels, i * self.pixels, self.pixels, self.pixels)
+                            self.fenetre.blit(self.image_chemin, rect)
+                            
+                        elif self.terrain[i][j] == 1:
+                            rect = pygame.Rect(j * self.pixels, i * self.pixels, self.pixels, self.pixels)
+                            self.fenetre.blit(self.image_mur, rect)
+
+                        elif self.terrain[i][j] == 2:
+                            rect = pygame.Rect(j * self.pixels, i * self.pixels, self.pixels, self.pixels)
+                            self.fenetre.blit(self.image_menu, rect) 
+
+                self.fenetre.blit(self.image_monstre, (self.pos_monstre[1] * self.pixels, self.pos_monstre[0] * self.pixels))
+
                 pygame.display.update()
+                self.horloge.tick(self.vitesse_monstre)
 
             # Map 2
             elif self.etat == "jeu_map2":
--- test2.py
+++ test2.py
@@ -1,73 +1,86 @@
-# hauteur 12
-# horizontale 23
-# start 12, 3
-
 import pygame
+from word import *
 
-terrain = [
+    # hauteur 12
+    # horizontale 23
+    # start 12, 3
+    # start2 4, 0
+class BouleGame:
+    def __init__(self, ):
+        self.terrain = terrain
+        self.largeur_fenetre = 1000
+        self.hauteur_fenetre = 550
+        self.couleur_fond = (255, 255, 255)
+        self.pixels = 40
+        self.vitesse_boule = 20
 
-    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
+        pygame.init()
+        self.fenetre = pygame.display.set_mode((self.largeur_fenetre, self.hauteur_fenetre))
+        pygame.display.set_caption("Déplacement de boule")
+        self.horloge = pygame.time.Clock()
 
-]
+        self.image_boule = pygame.image.load('Assets/alien.png')
+        self.image_boule = pygame.transform.scale(self.image_boule, (self.pixels, self.pixels))
 
-largeur_fenetre = 500
-hauteur_fenetre = 500
+        self.image_mur = pygame.image.load('Assets/Armes/wals.png')
+        self.image_mur = pygame.transform.scale(self.image_mur, (self.pixels, self.pixels))
 
-couleur_fond = (255, 255, 255)
-couleur_boule = (255, 0, 0)
+        self.image_chemin = pygame.image.load('Assets/sable.jpg')
+        self.image_chemin = pygame.transform.scale(self.image_chemin, (self.pixels, self.pixels))
 
-taille_boule = 20
-vitesse_boule = 5
+        self.image_menu = pygame.image.load("Assets/Armes/Image 89 at frame 1copy .jpg")
+        self.image_menu = pygame.transform.scale(self.image_menu, (self.pixels, self.pixels))
+        self.pos_monstre = [11, 1]
+        self.positions_visitees = []
 
-pygame.init()
-fenetre = pygame.display.set_mode((largeur_fenetre, hauteur_fenetre))
-pygame.display.set_caption("Déplacement de boule")
-horloge = pygame.time.Clock()
+    def est_position_valide(self, position):
+        x, y = position
+        return 0 <= x < len(self.terrain) and 0 <= y < len(self.terrain[0]) and self.terrain[x][y] == 0
 
-image_boule = pygame.image.load('Assets/alien.png')
-image_boule = pygame.transform.scale(image_boule, (taille_boule, taille_boule))
-boule_rect = image_boule.get_rect()
+    def deplacer(self):
+        x, y = self.pos_monstre
 
-pos_monstre = [12, 3]
-positions_visitees = []
+        if x - 1 >= 0 and self.est_position_valide([x - 1, y]) and [x - 1, y] not in self.positions_visitees:
+            self.pos_monstre = [x - 1, y]  # Déplacer vers le haut
 
-def est_position_valide(position):
-    x, y = position
-    return 0 <= x < len(terrain) and 0 <= y < len(terrain[0]) and terrain[x][y] == 0
+        elif y + 1 < len(self.terrain[0]) and self.est_position_valide([x, y + 1]) and [x, y + 1] not in self.positions_visitees:
+            self.pos_monstre = [x, y + 1]  # Déplacer vers la droite
 
-terminer = False
-while not terminer:
-    for evenement in pygame.event.get():
-        if evenement.type == pygame.QUIT:
-            terminer = True
+        elif x + 1 < len(self.terrain) and self.est_position_valide([x + 1, y]) and [x + 1, y] not in self.positions_visitees:
+            self.pos_monstre = [x + 1, y]  # Déplacer vers le bas
 
-    x, y = pos_monstre
+        elif y - 1 >= 0 and self.est_position_valide([x, y - 1]) and [x, y - 1] not in self.positions_visitees:
+            self.pos_monstre = [x, y - 1]  # Déplacer vers la gauche
 
-    if x - 1 >= 0 and est_position_valide([x - 1, y]) and [x - 1, y] not in positions_visitees:
-        pos_monstre = [x - 1, y]  # Déplacer vers le haut
-    elif x + 1 < len(terrain) and est_position_valide([x + 1, y]) and [x + 1, y] not in positions_visitees:
-        pos_monstre = [x + 1, y]  # Déplacer vers le bas
-    elif y + 1 < len(terrain[0]) and est_position_valide([x, y + 1]) and [x, y + 1] not in positions_visitees:
-        pos_monstre = [x, y + 1]  # Déplacer vers la droite
-    elif y - 1 >= 0 and est_position_valide([x, y - 1]) and [x, y - 1] not in positions_visitees:
-        pos_monstre = [x, y - 1]  # Déplacer vers la gauche
+    def run(self):
+        terminer = False
+        while not terminer:
+            for evenement in pygame.event.get():
+                if evenement.type == pygame.QUIT:
+                    terminer = True
 
-    positions_visitees.append([x, y])
-    fenetre.fill(couleur_fond)
-    boule_rect.topleft = (pos_monstre[1] * taille_boule, pos_monstre[0] * taille_boule)
-    fenetre.blit(image_boule, boule_rect)
-    pygame.display.flip()
-    horloge.tick(vitesse_boule)
-pygame.quit()
+            self.deplacer()
+            self.positions_visitees.append(self.pos_monstre)
+            self.fenetre.fill(self.couleur_fond)
+
+            for i in range(len(self.terrain)):
+                for j in range(len(self.terrain[0])):
+                    if self.terrain[i][j] == 1:
+                        rect = pygame.Rect(j * self.pixels, i * self.pixels, self.pixels, self.pixels)
+                        self.fenetre.blit(self.image_mur, rect)
+                    elif self.terrain[i][j] == 0:
+                        rect = pygame.Rect(j * self.pixels, i * self.pixels, self.pixels, self.pixels)
+                        self.fenetre.blit(self.image_chemin, rect)
+                    elif self.terrain[i][j] == 2:
+                        rect = pygame.Rect(j * self.pixels, i * self.pixels, self.pixels, self.pixels)
+                        self.fenetre.blit(self.image_menu, rect)
+
+            self.fenetre.blit(self.image_boule, (self.pos_monstre[1] * self.pixels, self.pos_monstre[0] * self.pixels))
+
+            pygame.display.flip()
+            self.horloge.tick(self.vitesse_boule)
+
+        pygame.quit()
+
+game = BouleGame()
+game.run()
