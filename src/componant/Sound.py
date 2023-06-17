7a17c8ab69b719b5459a6fb0b2d8b04df4dc47c3
Victor Yang <yangv140@gmail.com>
2023-06-17 12:43:27 +0200

    lancement automatique de la musique

--- src/componant/Sound.py
+++ src/componant/Sound.py
@@ -1,12 +1,15 @@
 import pygame
 
-
 class Sound:
     def __init__(self, nom_musique):
         pygame.mixer.init()
         self.nom_musique = nom_musique
         self.charger = False
         self.play = False
+        self.volume = 0.1
+
+        self.load_sound()
+        self.play_sound()
 
     def load_sound(self):
         pygame.mixer.music.load(self.nom_musique)
@@ -15,6 +18,7 @@ class Sound:
     def play_sound(self, loop=-1):
         if not self.charger:
             self.load_sound()
+        pygame.mixer.music.set_volume(self.volume)
         pygame.mixer.music.play(loop)
         self.play = True
 
