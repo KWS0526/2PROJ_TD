4d2fb24b1505d8c85e132e801e1cb5f90b1dd3fa
Victor Yang <yangv140@gmail.com>
2023-06-18 10:26:32 +0200

    optimisation des projectiles

--- src/componant/Projectille.py
+++ src/componant/Projectille.py
@@ -4,56 +4,40 @@ import pygame
 from pygame.sprite import Sprite
 from src.Assets.const import *
 
-
-class Projectile(Sprite):
-
-    def __init__(self, start_position_x, start_position_y, target_position, image,speed=0):
+class Projectile(pygame.sprite.Sprite):
+    def __init__(self, start_position_x, start_position_y, target_position, image, speed=4):
         super().__init__()
         self.start_position_x = start_position_x
         self.start_position_y = start_position_y
         self.target_position = target_position
         self.image = image
-        # self.image = pygame.image.load("Assets/image/Bullet1.png")
+
+        # Redimensionne l'image du projectile à une taille de 10x10 pixels
         self.image = pygame.transform.scale(self.image, (10, 10)).convert_alpha()
         self.rect = self.image.get_rect(center=(start_position_x, start_position_y))
-        dx = target_position[0] - start_position_x
-        dy = target_position[1] - start_position_y
-        distance = math.sqrt(dx ** 2 + dy ** 2)
-
-        if distance != 0:
-            direction_x = dx / distance
-            direction_y = dy / distance
-        else:
-            direction_x = 0
-            direction_y = 0
-
-        #  la vitesse du projectile
         self.speed = speed
 
-        self.velocity_x = direction_x * self.speed
-        self.velocity_y = direction_y * self.speed
+        # Calcul des distances et directions entre la position de départ et la position cible
+        self.dx = target_position[0] - start_position_x
+        self.dy = target_position[1] - start_position_y
+        self.distance = math.sqrt(self.dx ** 2 + self.dy ** 2)
 
-    def updates(self, monster_position):
-        # Calculer la direction entre le projectile et le monstre
-        dx = monster_position[0] - self.start_position_x
-        dy = monster_position[1] - self.start_position_y
-        # Normaliser la direction pour obtenir un vecteur unitaire
-        direction_length = math.sqrt(dx ** 2 + dy ** 2)
-        if direction_length != 0:
-            direction_x = dx / direction_length
-            direction_y = dy / direction_length
-            # Mettre à jour la position du projectile en fonction de la direction
-            speed = 2  # Vitesse du projectile (à ajuster selon vos besoins)
-            self.start_position_x += direction_x * speed
-            self.start_position_y += direction_y * speed
+        if self.distance != 0:
+            # Calcul des directions normalisées du projectile
+            self.direction_x = self.dx / self.distance
+            self.direction_y = self.dy / self.distance
+        else:
+            self.direction_x = 0
+            self.direction_y = 0
 
     def update(self):
-        self.start_position_x += self.velocity_x
-        self.start_position_y += self.velocity_y
+        # Mise à jour de la position du projectile en fonction de sa direction et de sa vitesse
+        self.start_position_x += self.direction_x * self.speed
+        self.start_position_y += self.direction_y * self.speed
         self.rect.center = (self.start_position_x, self.start_position_y)
 
     def draw(self, screen):
-        # Dessiner le projectile à l'écran
+        # Dessine le projectile sur l'écran
         screen.blit(self.image, self.rect)
 
     def detecter_collision_monstres(self, monstres):
@@ -61,13 +45,16 @@ class Projectile(Sprite):
             if self.rect.colliderect(monstre.rect):
                 degats = 5
                 if monstre.defense > 0:
-                    reduction_degats = int(degats * monstre.defense / 100)  # Réduction des dégâts en fonction de la défense
+                    # Réduction des dégâts en fonction de la défense du monstre
+                    reduction_degats = int(degats * monstre.defense / 100)
                     degats -= reduction_degats
 
+                # Inflige des dégâts au monstre et supprime le projectile
                 monstre.nbr_vie -= degats
-                self.kill()  # Supprimer le projectile lorsqu'il touche un monstre
+                self.kill()
                 print("Projectile touché un monstre")
 
                 if monstre.nbr_vie <= 0:
+                    # Supprime le monstre si ses points de vie atteignent zéro ou moins
                     print(monstre.nbr_vie)
-                    monstre.kill()  # Supprimer le monstre lorsqu'il est mort
+                    monstre.kill()
